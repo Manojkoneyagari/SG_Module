35ba89039f00c00d74ab37faa1b9679adecd2a4b
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-06-25 18:09:29 +0530

    vpc

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "Roboshop-sg" {
+  count = length(var.instances)
+  name        = "${var.instances[count.index]}-${var.project}-${environment}"
+  description = "Allowing traffic for ${var.instances[count.index]} for ${var.project} in ${var.environment}"
+  vpc_id      = aws_vpc.main.id
+
+
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = merge (
+    local.common_tags,{
+    Name = "${var.instances[count.index]}-${local.common_name}",
+    component = "${var.instances[count.index]}"
+    }
+  )
+}
